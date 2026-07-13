4a71411f6bced71adaf840585df2b7b3cca6c943
Lord-DG-003 <159617619+Lord-DG-003@users.noreply.github.com>
2026-07-14 02:56:44 +0530

    Search box is updated

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react'
+import '../App.css'
 
 const SearchBox = ({id}) => {
 
   const [query, setQuery]=useState("");
   const [results, setResults]=useState([]);
+  const [isLoading, setIsLoading]=useState("");
 
   useEffect(()=>{
 
     if(query.trim()===''){
-      setResults([])
+      setResults([]);
+      setIsLoading(query.trim());
       return
     }
 
     const timeoutId=setTimeout(async() => {
       try {
-        const response=await fetch(`https://fruit-search.freecodecamp.rocks/api/fruits?q=${query}`);
+        const response=await fetch(`https://fruit-search.freecodecamp.rocks/api/fruits?q=${query.trim()}`);
         const data=await response.json();
         setResults(data.map(fruit=>fruit.name))
 
-        console.log(data)
+        setIsLoading(query.trim());
+
+        console.log(isLoading);
       } catch (error) {
         console.error("Error has occurred to fetch your data. Try again.")
       }
@@ -27,16 +32,42 @@ const SearchBox = ({id}) => {
     return()=>clearTimeout(timeoutId);
   },[query]);
 
+  const handleInput=(selectedFruit)=>{
+    setQuery(selectedFruit);
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    if(query.trim()==="") return;
+
+    const encodeQuery=encodeURIComponent(query.trim());
+    const searchURL=`https://www.google.com/search?q=${encodeQuery}`;
+
+    window.open(searchURL,'_blank','noopener,noreferrer')
+
+  }
+
   return (
     <div id="search-bar" className='mt-6' >
-        <input value={query} type="search" placeholder='Enter Fruit Names' id={id} className='border-[1.5px] rounded-[5px]' onChange={(e)=>setQuery(e.target.value)} />
-        <div className='max-h-50 w-full mt-2 overflow-auto'>
-          {results.length>0?
-          results.map(item=>(
-            <p key={item} id="fruits">{item}</p>
-          )):
-          "No results found"}
-        </div>
+
+        <form onSubmit={handleSubmit}>
+
+          <input value={query} type="search" placeholder='Enter Fruit Names' id={id} className='border-[1.5px] rounded-[5px]' onChange={(e)=>setQuery(e.target.value)} />
+
+          <div className='max-h-50 w-full mt-2 overflow-auto'>
+
+            {results.length>0?
+            results.map(item=>(
+              <p><button className='cursor-pointer' key={item} id="fruits" onClick={()=>handleInput(item)} type='button'>{item}</button></p>
+            )):
+            isLoading&&"No results found"}
+
+          </div>
+
+          <button className='my-4 border-2 h-fit w-40 flex items-center justify-center ml-2.25 btn-animation' type='submit'>Search</button>
+
+        </form>
     </div>
   )
 }
